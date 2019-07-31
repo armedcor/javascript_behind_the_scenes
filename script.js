baf87b46cc561baee8361d075ff8351ad3a4ce3f
Cormac Lawlor <armedcor@gmail.com>
2019-07-31 12:00:09 +0100

    Hoisting

---
+++ script.js
@@ -0,0 +1,28 @@
+/****************
+ * Hoisting
+ *
+ */
+
+// Fucnctions
+calculateAge(1965);
+
+function calculateAge(year) {
+  console.log(2016 - year);
+}
+
+// retirement(1965);
+
+var retirement = function(year) {
+  console.log(65 - (2019 - year));
+};
+
+// Variables
+console.log(age);
+var age = 23;
+
+function foo() {
+  var age = 65;
+  console.log(age);
+}
+foo();
+console.log(age);
