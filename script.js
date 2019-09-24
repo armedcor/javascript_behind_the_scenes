a88e9ec902218779b6b579e47daa7a1268da71d5
Cormac Lawlor <armedcor@gmail.com>
2019-09-25 00:46:54 +0100

    Added Git ignore file

--- script.js
+++ script.js
@@ -3,26 +3,36 @@
  *
  */
 
-// Fucnctions
+// Functions
 calculateAge(1965);
 
 function calculateAge(year) {
-  console.log(2016 - year);
+    console.log(2019 - year);
 }
 
 // retirement(1965);
 
-var retirement = function(year) {
-  console.log(65 - (2019 - year));
+var retirement = function (year) {
+    console.log(65 - (2019 - year));
 };
 
 // Variables
+
+
 console.log(age);
 var age = 23;
 
 function foo() {
-  var age = 65;
-  console.log(age);
+    var age = 65;
+    console.log(age);
 }
+
 foo();
 console.log(age);
+
+/***********
+ *
+ * Scoping
+ */
+
+
